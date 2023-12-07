d42220e882186839f0332e9dd9ee1ce315b53aa5
Guillaume <gillard.guillaume@gmail.com>
2023-12-07 23:20:21 +0100

    update of the main file

--- graphe.py
+++ graphe.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 ##############################
 ####### H2 and O2 Data #######
 ##############################
 
 time_graph1 = np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
-H2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
-O2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+Volume_of_H2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+Volume_of_O2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 
 ###############################
 ### Faradic Efficiency Data ###
@@ -17,6 +18,8 @@ O2 = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 time_graph2 = np.array([0.0, 0.5, 1.0, 1.5, 2.0, 2.5])
 theoretical_speed_of_production = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
 real_speed_of_production = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+faradic_efficiency = (real_speed_of_production / theoretical_speed_of_production) * 100
+
 
 ##############################
 ### Energy Efficiency Data ###
@@ -44,7 +47,9 @@ Energy_of_reaction = molar_enthalpy_of_water_dissociation * number_of_moles_of_g
 # 4) Energy Efficiency
 Energy_Efficiency = (Energy_of_reaction / Electrical_Consumption) * 100 # in percentage
 
+#######################################################
 ### Experience 1 Data (distance between electrodes) ###
+#######################################################
 
 # Constant parameters :
 pH_experience1 = 14
@@ -94,20 +99,108 @@ Voltage_experience3 = np.array([0.0, 0.0, 0.0, 0.0])
 pH_experience4 = 14
 distance_experience4 = 7 # in cm
 
+# Variable parameters :
+Voltage_experience4 = np.array([30, 20, 10, 5]) # in volts
 
 
+###########################################
+### Volume Evolution of H2 and O2 Plot ####
+###########################################
 
-# Plot the data
-plt.plot(time_graph1, H2, label='H2')
-plt.plot(time_graph1, O2, label='O2')
+plt.plot(time_graph1, Volume_of_H2, label='H2')
+plt.plot(time_graph1, Volume_of_O2, label='O2')
 
-# Customize the plot
 plt.xlabel('Time (s)')
 plt.ylabel('Volume (m\u00B3)')
-plt.title('Evolution of H2 and O2 Volume')
+plt.title('Volume Evolution of H2 and O2')
 plt.legend()
 plt.grid(True)
-
-# Show the plot
 plt.show()
-plt.savefig('H2_and_02_volume_evolution.png')
+plt.savefig('Volume_evolution_of_H2_and_02.png')
+
+#################################
+#### Faradic Efficiency Plot ####
+#################################
+
+plt.plot(time_graph2, faradic_efficiency, label='Faradic Efficiency')
+
+plt.xlabel('Time (s)')
+plt.ylabel('Faradic Efficiency (%)')
+plt.title('Faradic Efficiency Evolution')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Faradic_Efficiency_Evolution.png')
+
+#################################
+#### Energy Efficiency Plot #####
+#################################
+
+plt.plot(time_graph2, Energy_Efficiency, label='Energy Efficiency')
+
+plt.xlabel('Time (s)')
+plt.ylabel('Energy Efficiency (%)')
+plt.title('Energy Efficiency Evolution')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Energy_Efficiency_Evolution.png')
+
+#################################
+#### Experience 1 Plot ##########
+#################################
+
+plt.plot(value_of_distance, Voltage_experience1, label='Voltage')
+
+plt.xlabel('Distance (cm)')
+plt.ylabel('Voltage (V)')
+plt.title('Voltage Evolution in function of distance')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Voltage_Evolution_in_function_of_distance.png')
+
+
+#################################
+#### Experience 2 Plot ##########
+#################################
+
+plt.plot(pH_experience2, Voltage_experience2, label='Voltage')
+
+plt.xlabel('pH')
+plt.ylabel('Voltage (V)')
+plt.title('Voltage Evolution in function of pH')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Voltage_Evolution_in_function_of_pH.png')
+
+
+#################################
+#### Experience 3 Plot ##########
+#################################
+
+plt.plot(current_experience3, Voltage_experience3, label='Voltage')
+
+plt.xlabel('Current (A)')
+plt.ylabel('Voltage (V)')
+plt.title('Voltage Evolution in function of current')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Voltage_Evolution_in_function_of_current.png')
+
+
+#################################
+#### Experience 4 Plot ##########
+#################################
+
+plt.plot(Voltage_experience4, current_experience3, label='Current')
+
+plt.xlabel('Voltage (V)')
+plt.ylabel('Current (A)')
+plt.title('Current Evolution in function of voltage')
+plt.legend()
+plt.grid(True)
+plt.show()
+plt.savefig('Current_Evolution_in_function_of_voltage.png')
